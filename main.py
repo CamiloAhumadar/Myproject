37bd1c0ee05ddcf5d0b92c58f3422dfce6093288
CamiloAhumadar <camiloahumada1997@gmail.com>
2026-06-17 11:40:26 -0500

    Agregado login de usuarios

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager, asynccontextmanager
 from fastapi import FastAPI
 from config.database import Base, engine
+from controller import auth_controller, task_controller 
 
 from controller.auth_controller import router as auth_router
 
@@ -9,18 +10,18 @@ async def lifespan(app: FastAPI):
     Base.metadata.create_all(engine)
     yield
 
-# Creamos una instancia de la aplicación FastAPI
 app = FastAPI(
     title="My task proyect",
     version="0.1",
     lifespan=lifespan
-
 )
 
 app.include_router(auth_router)
 
+app.include_router(task_controller.router)
+
 @app.get("/")
 async def welcome():
-    return {"mensaje": "¡Bienvenido a mi primera API con FastAPI!"}
+    return {"mensaje": "¡Bienvenido a mi primera API con FastAPI!"}   
 
 
